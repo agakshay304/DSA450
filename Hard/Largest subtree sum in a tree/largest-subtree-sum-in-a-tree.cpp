e82bd6235ba92143a0ef7cfea9a5413ee669bbf0
Akshay Gupta <75080491+agakshay304@users.noreply.github.com>
2022-12-28 21:06:44 +0530

    Added Solution - GfG to GitHub

---
+++ Hard/Largest subtree sum in a tree/largest-subtree-sum-in-a-tree.cpp
@@ -0,0 +1,121 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+/* A binary tree node has data, pointer to left child
+   and a pointer to right child */
+struct Node {
+    int data;
+    struct Node* left;
+    struct Node* right;
+};
+Node* newNode(int val) {
+    Node* temp = new Node;
+    temp->data = val;
+    temp->left = NULL;
+    temp->right = NULL;
+    return temp;
+}
+Node* buildTree(string str) {
+    // Corner Case
+    if (str.length() == 0 || str[0] == 'N') return NULL;
+
+    // Creating vector of strings from input
+    // string after spliting by space
+    vector<string> ip;
+
+    istringstream iss(str);
+    for (string str; iss >> str;) ip.push_back(str);
+
+    // Create the root of the tree
+    Node* root = newNode(stoi(ip[0]));
+
+    // Push the root to the queue
+    queue<Node*> queue;
+    queue.push(root);
+
+    // Starting from the second element
+    int i = 1;
+    while (!queue.empty() && i < ip.size()) {
+
+        // Get and remove the front of the queue
+        Node* currNode = queue.front();
+        queue.pop();
+
+        // Get the current node's value from the string
+        string currVal = ip[i];
+
+        // If the left child is not null
+        if (currVal != "N") {
+
+            // Create the left child for the current node
+            currNode->left = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->left);
+        }
+
+        // For the right child
+        i++;
+        if (i >= ip.size()) break;
+        currVal = ip[i];
+
+        // If the right child is not null
+        if (currVal != "N") {
+
+            // Create the right child for the current node
+            currNode->right = newNode(stoi(currVal));
+
+            // Push it to the queue
+            queue.push(currNode->right);
+        }
+        i++;
+    }
+
+    return root;
+}
+
+
+// } Driver Code Ends
+//User function Template for C++
+
+class Solution {
+  public:
+   int post_order_traversal(Node * root, int * maxSum){
+    if(root == NULL)
+        return 0;
+    int left_value = post_order_traversal(root->left, maxSum);
+    int right_value = post_order_traversal(root->right, maxSum);
+    int currSum = root->data + left_value + right_value;
+    if(currSum > *maxSum)
+        *maxSum = currSum;
+    // Here we return the sum of current subtree not the maxSum
+    return currSum;
+}
+int findLargestSubtreeSum(Node* root)
+{
+    int maxSum = INT_MIN;
+    // Pass the address instead of value to save the value_changes in recursion
+    post_order_traversal(root, &maxSum);
+    return maxSum;
+}
+};
+
+
+//{ Driver Code Starts.
+
+/* Driver program to test size function*/
+int main() {
+    int t;
+    scanf("%d\n", &t);
+    while (t--) {
+        string s;
+        getline(cin, s);
+        Node* root = buildTree(s);
+        Solution ob;
+        cout << ob.findLargestSubtreeSum(root) << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
