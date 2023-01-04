e207047941f6b06327c942670ee7b427bf910067
Akshay Gupta <75080491+agakshay304@users.noreply.github.com>
2023-01-04 23:10:38 +0530

    Added solution - LeetHub

--- Edit Distance - GFG/edit-distance.cpp
+++ Edit Distance - GFG/edit-distance.cpp
@@ -5,17 +5,25 @@ using namespace std;
 // } Driver Code Ends
 class Solution {
   public:
+  
+  //Approach:
+  //1.The above problem is not a part of LCS.
+  //2.Because here one more operation that is replace char is added.
+  //3.In initialisation we have to initialise the 0 row or col with
+  //  its respective size (which represents number of insertion)
+  //4.In the main loop we will take the min of three i.e (insertion,deletion or replacement)
+  //5.Finally 1 is added to add one operation in either of the three cases
+  
+  
     int dp[1001][1001];
-    int LCS(string s,int n,string t,int m)
+    int solve(string s,int n,string t,int m)
     {
         //Initialisation
         for(int i=0;i<n+1;i++){
             for(int j=0;j<m+1;j++)
             {
                 if(j==0)
-                {
                     dp[i][j]=i;
-                }
                 if(i==0)
                 dp[i][j]=j;
             }
@@ -41,9 +49,7 @@ class Solution {
         memset(dp,-1,sizeof(dp));
        int n=s.size();
        int m=t.size();
-       int ans=LCS(s,n,t,m);
-       
-    //   return ((m-ans)+(n-ans));
+       int ans=solve(s,n,t,m);
     return ans;
     }
 };
